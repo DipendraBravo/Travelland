40de7fd35a6465f0c68bd7bbe9c2c79a6d681b28
Dipendra Tamang <dipendratamang9@gmail.com>
2020-02-05 21:50:30 +0545

    home about and sign in login checkout

--- travelland/pages/urls.py
+++ travelland/pages/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about')
-
+    path('about', views.about, name='about'),
+    path('destination', views.destination, name='destination'),
+    path('signin', views.signin, name='signin'),
 ]
